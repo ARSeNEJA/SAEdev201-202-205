c055787bfd36418cf747f7f3747f7ee0222615cc
Ethan CLEMENT <ethan.clement@etu.univ-lehavre.fr>
2026-06-04 13:49:17 +0200

    Ajout de Panel.java

---
+++ ApplicationJeu/ihm/PanelJeu.java
@@ -0,0 +1,14 @@
+package src.vue;
+
+import src.Controleur;
+
+import javax.swing.*;
+
+public class PanelJeu extends JPanel
+{
+	public PanelJeu ( Controleur ctrl )
+	{
+		
+		this.add(new JLabel("Test 1"));
+	}
+}
