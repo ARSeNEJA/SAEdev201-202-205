c75049bff2e4a83ca8c3e8f19157450978b1a2f0
Alexis Guimbard <alexis.guimbard@etu.univ-lehavre.fr>
2026-06-05 15:48:50 +0200

    Modif PanelJeu.java

--- ApplicationJeu/src/ihm/PanelJeu.java
+++ ApplicationJeu/src/ihm/PanelJeu.java
@@ -3,7 +3,7 @@ package src.ihm;
 import src.Controleur;
 
 import java.awt.BorderLayout;
-
+import java.awt.*;
 import javax.swing.*;
 
 public class PanelJeu extends JPanel
@@ -19,36 +19,45 @@ public class PanelJeu extends JPanel
 		
 		panelPlateau = new JPanel();
 		
-		@Override
-		protected void paintComponent(Graphics g) 
-		{
-			super.paintComponent(g); 
-			
-			Graphics2D g2 = (Graphics2D) g;
-            g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-
-            // Dessin d'une grille témoin
-            g2.setColor(new Color(220, 220, 220));
-            int tailleCase = 60;
-            for (int i = 0; i < this.getWidth(); i += tailleCase) {
-                g2.drawLine(i, 0, i, this.getHeight());
-            }
-            for (int j = 0; j < this.getHeight(); j += tailleCase) {
-                g2.drawLine(0, j, this.getWidth(), j);
-            }
-
-            // Texte de substitution au centre
-            g2.setColor(Color.GRAY);
-            g2.setFont(new Font("Arial", Font.BOLD, 16));
-            String message = "[ Emplacement du Plateau Moléculaire ]";
-            int strWidth = g2.getFontMetrics().stringWidth(message);
-            g2.drawString(message, (this.getWidth() - strWidth) / 2, this.getHeight() / 2);
-        }
-        
-
-
+		// PANNEL de JEu ( panneau central )
         panelPlateau.setBackground(new Color(250, 250, 250));
         panelPlateau.setBorder(BorderFactory.createLineBorder(Color.DARK_GRAY, 2));
 
+                
+        // PANNEL INFORMATION - HUD ( Zone gauche )
+        panelInformation = new JPanel();
+        panelInformation.setLayout(new BoxLayout(panelInformation, BoxLayout.Y_AXIS));
+        panelInformation.setSize(320, 0);
+
+        // Zone deck
+
+
+        // Zone carte defausse
+
+
+        //Zone des manche 
+
+        this.add(panelPlateau, BorderLayout.CENTER);
+        this.add(panelInformation, BorderLayout.WEST);
 	}
+
+    protected void paintComponent(Graphics g) 
+	{
+		super.paintComponent(g); 
+			
+		Graphics2D g2 = (Graphics2D) g;
+        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+
+        // Dessin d'une grille témoin
+        g2.setColor(new Color(220, 220, 220));
+        int tailleCase = 60;  //a modifier en fonction de la taille du plateau
+        for (int i = 0; i < this.getWidth(); i += tailleCase) {
+            g2.drawLine(i, 0, i, this.getHeight());
+        }
+        for (int j = 0; j < this.getHeight(); j += tailleCase) {
+            g2.drawLine(0, j, this.getWidth(), j);
+        }
+
+            
+    }
 }
